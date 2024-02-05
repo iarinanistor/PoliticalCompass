9a72233c9faafaecdb348317ebe3b659f88be3a9
Nistor Iarina <iarina.nistor@etu.sorbonne-universite.fr>
2024-02-05 12:34:26 +0000

    Replace votes.py

--- ProjetVote/votes.py
+++ ProjetVote/votes.py
@@ -1,34 +1,59 @@
-from Candidat import *
+
+import bib_objet as ob
+
 def un_seul_vainqueur(vainqueurs):
-    '''vainqueurs : List[Candidat] -> Candidat
-        retourne le candidat le plus agee et le plus charismatic
     '''
-    return max(vainqueurs, key = lambda x : (x.age, x.charisme)) 
+    Parameters:
+        vainqueurs : List[Candidat]
+    Returns:
+        Candidat: le candidat le plus agee et le plus charismatic
+    '''
+    return max(vainqueurs, key = lambda x : (x.age(), x.charisme())) 
 
 def comptage_majorite(electeurs) :
-    ''' electeurs : List[Electeur] -> int
-        retourne la majorite d'une liste contenant n elements
+    ''' 
+    Paramaters:
+        electeurs : List[Individus] 
+    Returns:
+        int : la majorite d'une liste contenant n elements
     '''
     nb_electeurs = len(electeurs)
     return nb_electeurs//2 + 1 
 
+def is_majority(n,electeurs) :
+    '''
+    Parameters:
+        n:int
+        electeurs : List[Individus]
+    Returns:
+        bool : true si n est plus grand que la moitie de longueur de liste d'electeurs
+    '''
+    return n > (len(electeurs)/2)
+
 def comptage_votes(candidats,electeurs):
-    ''' candidats : List[Candidat] electeurs : List[Electeur] -> Dict{Candidat:int}
-        retourne une dictionnaire avec chaque candidat et son nb de votes
+    ''' 
+    Parameters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus]
+    Returns:
+        Dict{Candidat:int} : dictionnaire avec chaque candidat et son nb de votes-etant le premier classe
+         
     '''
     nb_votes = {candidate:0 for candidate in candidats}
 
     for electeur in electeurs :
-        nb_votes[electeur.liste_a_completer[0]] += 1
-        """a changer apres : la liste des candidats que l'electeur a choisi (ordonnee)"""
-        """ il peut etre un attribut ou le resultat d'une methode"""
+        nb_votes[(electeur.liste_vote())[0]] += 1
 
     return nb_votes
 
 def pluralite(candidats,electeurs):
-    '''candidats : List[Candidat] electeurs : List[Electeur] -> Candidat
-        retourne le candidat gagnant par la regle de vote PLURALITE
-        les critere de departage : age, charisme
+    '''
+    Parameters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus] 
+    Returns:
+        Candidat : le candidat gagnant par la regle de vote PLURALITE
+                    les critere de departage : age, charisme
     '''
     nb_votes = comptage_votes(candidats,electeurs)
     max_vote = max(nb_votes.values())
@@ -36,18 +61,20 @@ def pluralite(candidats,electeurs):
     return un_seul_vainqueur(vainqueurs)
 
 def borda(candidats,electeurs):
-    '''candidats : List[Candidat] electeurs : List[Electeur] -> Candidat
-        retourne le candidat gagnant par la regle de vote BORDA
-        les critere de departage : age, charisme
+    '''
+    Paramaters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus] 
+    Returns:    
+        Candidat : le candidat gagnant par la regle de vote BORDA
+        les criteres de departage : age, charisme
     '''
     nb_votes = {candidate:0 for candidate in candidats}
     n = len(candidats)
 
     for electeur in electeurs :
         for i in range(n):
-            nb_votes[electeur.liste_a_completer[i]] += n-1-i
-        """a changer apres : la liste des candidats que l'electeur a choisi (ordonnee)"""
-        """ il peut etre un attribut ou le resultat d'une methode"""
+            nb_votes[electeur.liste_vote()[i]] += n-1-i
 
     max_vote = max(nb_votes.values())
     vainqueurs = [candidate for candidate,score in nb_votes.items() if score == max_vote]
@@ -55,40 +82,45 @@ def borda(candidats,electeurs):
     return un_seul_vainqueur(vainqueurs)
 
 def stv(candidats,electeurs):
-    '''candidats : List[Candidat] electeurs : List[Electeur] -> Candidat
-        retourne le candidat gagnant par la regle de vote STV
-        les critere de departage : age, charisme
+    '''Paramaters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus] 
+    Returns:    
+        Candidat : le candidat gagnant par la regle de vote STV
+        les criteres de departage : age, charisme
     '''
 
-    majorite = comptage_majorite(electeurs)
-
+    l_candidat = candidats
+    l_electeur = electeurs
     while True :
-        nb_votes = comptage_votes(candidats,electeurs)
+        nb_votes = comptage_votes(l_candidat,l_electeur)
 
-        for candidate,votes in votes.items():
-            if votes >= majorite :
+        for candidate,votes in nb_votes.items():
+            if is_majority(votes,l_electeur):
                 return candidate
             
         candidate_elimine = min(nb_votes, key = nb_votes.get)
         
-        for electeur in electeurs :
-            (electeur.liste_a_completer).remove(candidate_elimine)
-        """a changer apres : la liste des candidats que l'electeur a choisi (ordonnee)"""
-        """ il peut etre un attribut ou le resultat d'une methode"""
+        for electeur in l_electeur :
+            (electeur.liste_vote()).remove(candidate_elimine)
+        l_candidat.remove(candidate_elimine)
+        
 
 
-def approbation(candidats,electeurs):
-    '''candidats : List[Candidat] electeurs : List[Electeur] -> Candidat
-        retourne le candidat gagnant par la regle de vote APPROBATION
-        les critere de departage : age, charisme
+def approbation(candidats,electeurs,nb_approbation):
+    '''Paramaters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus] 
+        nb_approbation : int
+    Returns:    
+        Candidat : le candidat gagnant par la regle de vote APPROBATION avec les nb_approbation premiers canddiats clasees
+        les criteres de departage : age, charisme
     '''
     nb_votes = {candidate:0 for candidate in candidats}
 
     for electeur in electeurs :
-        nb_approbation = electeur.approb 
-        '''si on ajoute une attribut dans la classe electeur et aussi une contraine qu'elle soit <= len(candidats)'''
         for i in range(nb_approbation):
-            nb_votes[electeur.liste_a_completer[i]] += 1
+            nb_votes[electeur.liste_vote()[i]] += 1
         """a changer apres : la liste des candidats que l'electeur a choisi (ordonnee)"""
         """ il peut etre un attribut ou le resultat d'une methode"""
 
