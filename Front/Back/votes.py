32fbec77eb731737c82d33a7cc1c30de7ea7b247
Zhou Jeremy <jeremy.zhou.2@etu.sorbonne-universite.fr>
2024-03-05 15:01:31 +0000

    Update votes.py
    Ajout des signatures des fonctions

--- Front/Back/votes.py
+++ Front/Back/votes.py
@@ -3,17 +3,19 @@ def un_seul_vainqueur(vainqueurs):
     '''
     Parameters:
         vainqueurs : List[Candidat]
+
     Returns:
-        Candidat: le candidat le plus agee et le plus charismatic
+        Candidat: le candidat le plus agee et le plus charismatique
     '''
     return max(vainqueurs, key = lambda x : (x.age(), x.charisme())) 
 
 def comptage_majorite(electeurs) :
     ''' 
-    Paramaters:
+    Parameters:
         electeurs : List[Individus] 
+
     Returns:
-        int : la majorite d'une liste contenant n elements
+        int: la majorite d'une liste contenant n elements
     '''
     nb_electeurs = len(electeurs)
     return nb_electeurs//2 + 1 
@@ -23,8 +25,9 @@ def is_majority(n,electeurs) :
     Parameters:
         n:int
         electeurs : List[Individus]
+
     Returns:
-        bool : true si n est plus grand que la moitie de longueur de liste d'electeurs
+        bool: True si n est plus grand que la moitie de longueur de liste d'electeurs
     '''
     return n > (len(electeurs)/2)
 
@@ -33,9 +36,9 @@ def comptage_votes(candidats,electeurs):
     Parameters:
         candidats : List[Candidat] 
         electeurs : List[Individus]
+
     Returns:
         Dict{Candidat:int} : dictionnaire avec chaque candidat et son nb de votes-etant le premier classe
-         
     '''
     nb_votes = {candidate:0 for candidate in candidats}
 
@@ -48,10 +51,11 @@ def pluralite(candidats,electeurs):
     '''
     Parameters:
         candidats : List[Candidat] 
-        electeurs : List[Individus] 
+        electeurs : List[Individus]
+
     Returns:
         Candidat : le candidat gagnant par la regle de vote PLURALITE
-                    les critere de departage : age, charisme
+                   les criteres de departage : age, charisme
     '''
     nb_votes = comptage_votes(candidats,electeurs)
     max_vote = max(nb_votes.values())
@@ -60,12 +64,13 @@ def pluralite(candidats,electeurs):
 
 def borda(candidats,electeurs):
     '''
-    Paramaters:
+    Parameters:
         candidats : List[Candidat] 
         electeurs : List[Individus] 
+
     Returns:    
         Candidat : le candidat gagnant par la regle de vote BORDA
-        les criteres de departage : age, charisme
+                   les criteres de departage : age, charisme
     '''
     nb_votes = {candidate:0 for candidate in candidats}
     n = len(candidats)
@@ -80,12 +85,14 @@ def borda(candidats,electeurs):
     return un_seul_vainqueur(vainqueurs)
 
 def stv(candidats,electeurs):
-    '''Paramaters:
+    '''
+    Parameters:
         candidats : List[Candidat] 
         electeurs : List[Individus] 
+    
     Returns:    
         Candidat : le candidat gagnant par la regle de vote STV
-        les criteres de departage : age, charisme
+                   les criteres de departage : age, charisme
     '''
 
     l_candidat = candidats
@@ -103,40 +110,18 @@ def stv(candidats,electeurs):
             (electeur.liste_vote()).remove(candidate_elimine)
         l_candidat.remove(candidate_elimine)
         
-#a tester sur l'interface graphique      
-def stv1(candidats,electeurs):
-    '''Paramaters:
-        candidats : List[Candidat] 
-        electeurs : List[Individus] 
-    Returns:    
-        Candidat : le candidat gagnant par la regle de vote STV
-        les criteres de departage : age, charisme
-    '''
 
-    l_candidat = candidats
-    l_electeur = electeurs
-    dico={electeur:electeur.liste_vote() for electeur in electeurs}
-    while True :
-        nb_votes = comptage_votes(l_candidat,l_electeur)
-
-        for candidate,votes in nb_votes.items():
-            if is_majority(votes,l_electeur):
-                return candidate
-            
-        candidate_elimine = min(nb_votes, key = nb_votes.get)
-        
-        for electeur in dico :
-            dico[electeur].remove(candidate_elimine)
-        l_candidat.remove(candidate_elimine)
 
 def approbation(candidats,electeurs,nb_approbation):
-    '''Paramaters:
+    '''
+    Parameters:
         candidats : List[Candidat] 
         electeurs : List[Individus] 
         nb_approbation : int
+
     Returns:    
-        Candidat : le candidat gagnant par la regle de vote APPROBATION avec les nb_approbation premiers canddiats clasees
-        les criteres de departage : age, charisme
+        Candidat : le candidat gagnant par la regle de vote APPROBATION avec les nb_approbation premiers candidats classes
+                   les criteres de departage : age, charisme
     '''
     nb_votes = {candidate:0 for candidate in candidats}
 
@@ -152,6 +137,14 @@ def approbation(candidats,electeurs,nb_approbation):
     return un_seul_vainqueur(vainqueurs)
 
 def battleOneToOne(candidats,electeurs):
+    '''
+    Paramaters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus]
+
+    Returns:    
+        Dico{(Candidat, Candidat):int} : le dictionnaire associant les duels entre chaque candidat et le score du candidat 1 contre le candidat 2
+    '''
     pairs_votes = {(c1,c2):0 for c1 in candidats for c2 in candidats if c1!=c2}
 
     for electeur in electeurs:
@@ -162,6 +155,15 @@ def battleOneToOne(candidats,electeurs):
     return pairs_votes
 
 def vainqueurCondorcet(candidats,electeurs):
+    '''
+    Paramaters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus]
+
+    Returns:    
+        Candidat: le vainqueur de Condorcet (si il y en a un)
+        None: si pas de vainqueur de Condorcet
+    '''
     pairs_votes = battleOneToOne(candidats,electeurs)
     for candidat in candidats:
         gagne = 0
@@ -175,6 +177,14 @@ def vainqueurCondorcet(candidats,electeurs):
     return None
 
 def copeland(candidats,electeurs):
+    '''
+    Paramaters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus]
+
+    Returns:    
+        Candidat: le candidat gagnant selon la règle de Copeland
+    '''
     nb_votes = {candidate:0 for candidate in candidats}
     pairs_votes = battleOneToOne(candidats,electeurs)
     for candidat in candidats:
@@ -189,6 +199,14 @@ def copeland(candidats,electeurs):
     return un_seul_vainqueur(vainqueurs)
 
 def simpson(candidats,electeurs):
+    '''
+    Paramaters:
+        candidats : List[Candidat] 
+        electeurs : List[Individus]
+
+    Returns:    
+        Candidat: le candidat gagnant selon la règle de Simpson
+    '''
     nb_loss = {candidate:0 for candidate in candidats}
     pairs_votes = battleOneToOne(candidats,electeurs)
     for candidat in candidats:
