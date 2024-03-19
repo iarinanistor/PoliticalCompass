1e973e98b2632a14f55897c5c89812a330427c06
Kinane-Daouadji Isaac <isaac.kinane-daouadji@etu.sorbonne-universite.fr>
2024-03-19 16:59:09 +0000

    Update 14 files
    - /Back/Individus.py
    - /Back/votes.py
    - /Back/Map.py
    - /Back/Candidat.py
    - /Back/Statistique/MonteCarlo.py
    - /Back/Statistique/__pycache__/MonteCarlo.cpython-39.pyc
    - /Back/__pycache__/Individus.cpython-39.pyc
    - /Back/__pycache__/Map.cpython-311.pyc
    - /Back/__pycache__/votes.cpython-311.pyc
    - /Back/__pycache__/Candidat.cpython-39.pyc
    - /Back/__pycache__/Individus.cpython-311.pyc
    - /Back/__pycache__/votes.cpython-39.pyc
    - /Back/__pycache__/Candidat.cpython-311.pyc
    - /Back/__pycache__/Map.cpython-39.pyc

---
+++ Back/Individus.py
@@ -0,0 +1,26 @@
+import numpy as np
+from random import uniform
+""" Pas de log pour les performences """
+class Individus:
+    def __init__(self, nom=None, x=None, y=None,liste_electeur=None):
+        self.nom = nom
+        self.x = x
+        self.y = y
+        self.map = map
+        self.liste_electeur = liste_electeur
+        self.poids = 1
+        self.c = uniform(0,10)
+        self.adelegue = False
+
+        #Getter pour obtenir le niveau de compétence de l'électeur
+    def get_c(self):
+        return self.c
+
+        #Getter pour obtenir le poids du vote de l'électeur
+    def get_poids(self):
+        return self.poids
+        
+    def liste_vote(self):
+        liste_des_votes = [(np.linalg.norm(np.array([i.x(), i.y()]) - np.array([self.x, self.y])), i) for i in self.liste_electeur]
+        l=sorted(liste_des_votes, key=lambda x: x[0])
+        return [ candidat for distance,candidat in l]
