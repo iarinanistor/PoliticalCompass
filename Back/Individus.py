887b746cb8ba3b8c8ace55f2a43bab61720371d5
Zhou Jeremy <jeremy.zhou.2@etu.sorbonne-universite.fr>
2024-03-19 16:03:09 +0000

    Replace Individus.py
    Ajout des attributs :
     - poids
     - c
     - adelegue
    
    Ajout des méthodes getters :
     - get_c()
     - get_poids()

--- Back/Individus.py
+++ Back/Individus.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import uniform
 """ Pas de log pour les performences """
 class Individus:
     def __init__(self, nom=None, x=None, y=None,liste_electeur=None):
@@ -7,6 +8,17 @@ class Individus:
         self.y = y
         self.map = map
         self.liste_electeur = liste_electeur
+        self.poids = 1
+        self.c = uniform(0,10)
+        self.adelegue = False
+
+        #Getter pour obtenir le niveau de compétence de l'électeur
+        def get_c(self):
+            return self.c
+
+        #Getter pour obtenir le poids du vote de l'électeur
+        def get_poids(self):
+            return self.poids
         
     def liste_vote(self):
         liste_des_votes = [(np.linalg.norm(np.array([i.x(), i.y()]) - np.array([self.x, self.y])), i) for i in self.liste_electeur]
