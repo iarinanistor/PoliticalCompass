36bc7e909112db00b0720a929afafb0bcdd5e594
Kinane-Daouadji Isaac <isaac.kinane-daouadji@etu.sorbonne-universite.fr>
2024-03-19 12:58:36 +0000

    Update 41 files
    - /Front/Utilitaire.py
    - /Front/Planete/Planete.py
    - /Front/Planete/__pycache__/Planete.cpython-39.pyc
    - /Front/Planete/Texture/final.png
    - /Front/Planete/Texture/cmp2.png
    - /Front/Planete/Texture/cmp.png
    - /Front/__pycache__/grilleKV.cpython-39.pyc
    - /Front/__pycache__/ListeRes.cpython-39.pyc
    - /Front/__pycache__/Utilitaire.cpython-39.pyc
    - /Front/__pycache__/MWindow.cpython-39.pyc
    - /Front/__pycache__/Boutons.cpython-39.pyc
    - /Front/__pycache__/MenuLateral.cpython-39.pyc
    - /Front/__pycache__/MainWindow.cpython-39.pyc
    - /Front/__pycache__/Base_Donnee.cpython-39.pyc
    - /Front/__pycache__/main.cpython-39.pyc
    - /Front/__pycache__/MapQT.cpython-39.pyc
    - /Front/__pycache__/SideBarre.cpython-39.pyc
    - /Front/__pycache__/Loading.cpython-39.pyc
    - /Front/__pycache__/Map.cpython-39.pyc
    - /Front/Windows/SWindow2.py
    - /Front/Windows/MWindow.py
    - /Front/Windows/SWindow.py
    - /Front/Windows/__pycache__/MWindow.cpython-39.pyc
    - /Front/Windows/__pycache__/SWindow.cpython-39.pyc
    - /Front/Widgets/testBouton.py
    - /Front/Widgets/Loading.py
    - /Front/Widgets/MapQT.py
    - /Front/Widgets/IO_Bouton.py
    - /Front/Widgets/ListeRes.py
    - /Front/Widgets/Boutons.py
    - /Front/Widgets/Pop_up.py
    - /Front/Widgets/MenuLateral.py
    - /Front/Widgets/__pycache__/ListeRes.cpython-39.pyc
    - /Front/Widgets/__pycache__/Boutons.cpython-39.pyc
    - /Front/Widgets/__pycache__/MenuLateral.cpython-39.pyc
    - /Front/Widgets/__pycache__/IO_Bouton.cpython-39.pyc
    - /Front/Widgets/__pycache__/MapQT.cpython-39.pyc
    - /Front/Widgets/__pycache__/Loading.cpython-39.pyc
    - /Front/Widgets/Texture/style.qss
    - /Front/Widgets/Texture/upload.png
    - /Front/Widgets/Texture/Bouton.qss

---
+++ Front/Widgets/MenuLateral.py
@@ -0,0 +1,82 @@
+import sys
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget, QLabel
+from Front.Widgets.Boutons import *
+class SideMenu(QMainWindow):
+    def __init__(self,bd,Blongeur = 300,Bhauteur = 40,tailleMap = 500):
+        """
+        Constructeur de la classe SideMenu.
+
+        Args:
+            bd (object): Objet représentant une base de données.
+            Blongeur (int): Longueur des boutons. Par défaut, 300.
+            Bhauteur (int): Hauteur des boutons. Par défaut, 40.
+            tailleMap (int): Taille de la carte. Par défaut, 500.
+        """
+        super().__init__()  
+        #taille boutton 
+        self.Blongeur = Blongeur
+        self.Bhauteur = Bhauteur
+        self.tailleMap = tailleMap
+        self.bd = bd
+        # Widget central pour afficher du texte
+        #self.central_widget = QLabel("Texte initial")
+        #self.setCentralWidget(self.central_widget)
+
+        # Layout principal
+        self.layout = QVBoxLayout()
+
+        # Bouton 1
+        Menu_layout = QVBoxLayout()
+        Menu = QPushButton("Menu")
+        Menu.setFixedSize(self.Blongeur,self.Bhauteur)
+        #self.layout.addWidget(Menu)
+        #  Boutoun de creation des candidat 
+        entreeCand = EntreeCandidat(self.bd,self.Blongeur,self.Bhauteur,self.tailleMap)
+        self.layout.addWidget(entreeCand)
+        
+        # Boutoun de creation des candidat aleatoirement
+        bouton_Genr = Boutoun_GenerAleatoire(self.bd,self.Blongeur,self.Bhauteur)
+        self.layout.addWidget(bouton_Genr)
+        
+         #boutoun Borda
+        bouton_Copeland  = Bouton_Mvote(self.bd,"Copeland", self.Blongeur,self.Bhauteur)
+        self.layout.addWidget(bouton_Copeland)
+        
+        #boutoun Borda
+        bouton_Borda = Bouton_Mvote(self.bd,"Borda", self.Blongeur,self.Bhauteur)
+        self.layout.addWidget(bouton_Borda)
+        
+        #boutoun Pluralite
+        bouton_Pluralite = Bouton_Mvote(self.bd,"Pluralite", self.Blongeur,self.Bhauteur)
+        self.layout.addWidget(bouton_Pluralite)
+        
+        #boutoun STV
+        bouton_STV = Bouton_Mvote(self.bd,"STV", self.Blongeur,self.Bhauteur)
+        self.layout.addWidget(bouton_STV)
+        
+        # bouton Approbation
+        bouton_Approbation = Bouton_Mvote(self.bd,"Approbation", self.Blongeur,self.Bhauteur)
+        self.layout.addWidget(bouton_Approbation)
+        # boutoun Save
+        bouton_save = BoutonSave(self.bd, self.Blongeur,self.Bhauteur)
+        self.layout.addWidget(bouton_save)
+        #boutoun_recgarge
+        bouton_recharge = BoutonRecharge(self.bd,self.Blongeur,self.Bhauteur)
+        self.layout.addWidget(bouton_recharge)
+       
+    
+
+        # Widget pour contenir les boutons du menu latéral
+        self.menu_widget = QWidget()
+        self.menu_widget.setLayout(self.layout)
+        self.setMenuWidget(self.menu_widget)
+
+        # Alignement du layout principal en haut à gauche
+        self.layout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SideMenu()
+    window.show()
+    sys.exit(app.exec())
